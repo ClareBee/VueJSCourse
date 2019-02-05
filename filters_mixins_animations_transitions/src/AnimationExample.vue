c7e8729747a08ee989a7f6aa6ff8a9530940053a
Clare <chblackburne@gmail.com>
2019-02-05 19:32:22 +0000

    basic transition example

--- filters_mixins_animations_transitions/src/AnimationExample.vue
+++ filters_mixins_animations_transitions/src/AnimationExample.vue
@@ -3,7 +3,7 @@
     <h1>Animations</h1>
     <button @click="show = !show" class="btn btn-primary">Show alert</button>
     <br /><br />
-    <transition>
+    <transition name="fade">
       <div v-if="show" class="alert alert-info">This is some info</div>
     </transition>
   </div>
@@ -18,4 +18,15 @@ export default {
 }
 </script>
 <style>
+  .fade-enter {
+    opacity: 0;
+  }
+  .fade-enter-active {
+    transition: opacity 1s;
+    /* no need to specify 1 as fade-enter gets removed and 1 is default */
+  }
+  .fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+  }
 </style>
--- vue_animation_and_transitions.md
+++ vue_animation_and_transitions.md
@@ -1,3 +1,8 @@
 ### Animations
 
 <transition> can only be used for a single element
+Adding css-classes (name-enter, name-enter-active, name-leave, name-leave-active -> defaults to v-enter)
+e.g. <transition name="fade">
+      <!-- html  -->
+      </transition>
+=> give you .fade-enter/.fade-enter-active/.fade-leave/.fade-leave-active for css
