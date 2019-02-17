d7ed10c1ca978fa5981540d38e67529fc3ed1339
Clare <chblackburne@gmail.com>
2019-02-17 18:26:07 +0000

    basic setup for nested routes

--- routing/src/components/user/User.vue
+++ routing/src/components/user/User.vue
@@ -4,22 +4,14 @@
     <p>{{ id }}</p>
     <hr>
     <button @click="navigateToHome" class="btn btn-info">Home</button>
+    <hr>
+    <router-view></router-view>
   </div>
 
 </template>
 
 <script>
   export default {
-    data(){
-      return {
-        id: this.$route.params.id
-      }
-    },
-    watch: {
-      $route(to, from){
-      this.id = to.params.id
-      }
-    }
     methods: {
       navigateToHome(){
         this.$router.push('/');
