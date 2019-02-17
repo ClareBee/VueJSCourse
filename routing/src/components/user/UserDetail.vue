a11216ae97fadda7d131e4332f2ae2d9b23b7215
Clare <chblackburne@gmail.com>
2019-02-17 18:38:26 +0000

    basic dynamic links w interpolation

--- routing/src/components/user/UserDetail.vue
+++ routing/src/components/user/UserDetail.vue
@@ -1,3 +1,7 @@
 <template>
+  <div>
     <h3>Some User Details</h3>
+    <p>Loaded User: id: {{ $route.params.id }} </p>
+    <router-link :to="'/user/' + $route.params.id + '/edit'" tag="button" class="btn btn-success">Edit</router-link>
+  </div>
 </template>
